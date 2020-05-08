74eb74e60c799a756088da5559d26aa9c914c46e
HANXU2018 <1076998404@qq.com>
2020-05-08 22:09:14 +0800

    连接数据库

--- VueNodeJsStudy/Vue/back/index.js
+++ VueNodeJsStudy/Vue/back/index.js
@@ -1,5 +1,5 @@
 var http = require("http"); //加载内置 http 模块，无需安装
-
+var db = require("./db");
 http
   .createServer(function(request, response) {
     response.writeHead(200, { "Content-Type": "text/plain" });
