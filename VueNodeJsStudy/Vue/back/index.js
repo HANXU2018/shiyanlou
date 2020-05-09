b4e33e2d9202ffd9097e299674b36a4cb274f69a
HANXU2018 <1076998404@qq.com>
2020-05-09 15:12:05 +0800

    try code Middleware instance

--- VueNodeJsStudy/Vue/back/index.js
+++ VueNodeJsStudy/Vue/back/index.js
@@ -1,25 +1,41 @@
 const Koa = require("koa");
 const app = new Koa();
-let middle1 = async (ctx, next) => {
-  if (ctx.path == "/favicon.ico") return;
-  console.log("1 before");
-  await next();
-  console.log("1 after");
-  ctx.body = "Hello Koa Middleware.";
-};
-let middle2 = async (ctx, next) => {
-  console.log("2 before");
-  await next();
-  console.log("2 after");
-};
-let middle3 = async (ctx, next) => {
-  console.log("3 before");
-  await next();
-  console.log("3 after");
-};
 
-app.use(middle1);
-app.use(middle2);
-app.use(middle3);
+// 全局错误捕获
+app.use(async (ctx, next) => {
+  try {
+    await next();
+    console.log("handler 通过");
+  } catch (err) {
+    console.log("handler 处理错误");
+    ctx.response.status = err.statusCode || err.status || 500;
+    ctx.response.body = {
+      message: err.message
+    };
+  }
+});
+
+// 记录请求地址和时间
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  console.log(`${ctx.method} ${ctx.url} in at ${start}.`);
+  await next();
+  const end = Date.now();
+  console.log(
+    `${ctx.method} ${ctx.url} out at ${end}.Time use: ${end - start}`
+  );
+});
+
+// 路由
+app.use(async (ctx, next) => {
+  if (ctx.path == "/login") {
+    ctx.body = "login";
+  } else if (ctx.path == "/signup") {
+    ctx.body = "signup";
+  } else {
+    await next();
+  }
+});
+
 app.listen(8082);
 console.log("Server running at http://127.0.0.1:8082/");
